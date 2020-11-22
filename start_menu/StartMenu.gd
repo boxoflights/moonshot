0f969ec908d37ec5462dc07c71e5549353dca9f6
Liam Kerr <liam.d.kerr@gmail.com>
2020-11-22 02:27:54 +0000

    feat: adding audio controls

--- start_menu/StartMenu.gd
+++ start_menu/StartMenu.gd
@@ -6,15 +6,22 @@ enum views {
 }
 
 var current_view = views.MAINMENU
+var music = load("res://moonshot-theme.ogg")
 
-# Called when the node enters the scene tree for the first time.
+var init
+
 func _ready():
 	set_view(views.MAINMENU)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	SoundManager._apply_audio_settings()
+	if(!SoundManager.is_music_playing()):
+		SoundManager.play_music(music)
+	$OptionsMenu/Buttons/Master/HSlider.value = SettingsManager.get_setting("master")
+	$OptionsMenu/Buttons/Music/HSlider.value = SettingsManager.get_setting("music")
+	$OptionsMenu/Buttons/SFX/HSlider.value = SettingsManager.get_setting("sfx")
+	$OptionsMenu/Buttons/Master/HSlider.connect("value_changed",self,"_on_Volume_value_changed")
+	$OptionsMenu/Buttons/Music/HSlider.connect("value_changed",self,"_on_Volume_value_changed")
+	$OptionsMenu/Buttons/SFX/HSlider.connect("value_changed",self,"_on_Volume_value_changed")
+	print(SettingsManager.get_setting("music"))
 
 func set_view(new_view):
 	match new_view:
@@ -26,6 +33,7 @@ func set_view(new_view):
 			current_view = new_view
 
 func _on_OPTIONS_pressed():
+	SettingsManager.save_settings()
 	set_view(views.OPTIONS)
 
 func _on_OptionsBack_pressed():
@@ -33,7 +41,7 @@ func _on_OptionsBack_pressed():
 
 func _on_PLAY_pressed():
 	$Fade/Tween.interpolate_property($Fade,"modulate",
-	Color(0,0,0,0),Color(0,0,0,1),0.5)
+		Color(0,0,0,0),Color(0,0,0,1),0.5)
 	$Fade/Tween.interpolate_callback(self,1,"_start_game")
 	$Fade/Tween.start()
 
@@ -43,3 +51,11 @@ func _on_QUIT_pressed():
 func _start_game():
 	$Fade.modulate = Color(0,0,0,0)
 	get_tree().change_scene("res://Main.tscn")
+
+
+func _on_Volume_value_changed(value):
+	SettingsManager.set_setting("master",$OptionsMenu/Buttons/Master/HSlider.value)
+	SettingsManager.set_setting("music",$OptionsMenu/Buttons/Music/HSlider.value)
+	SettingsManager.set_setting("sfx",$OptionsMenu/Buttons/SFX/HSlider.value)
+	SettingsManager.save_settings()
+	SoundManager._apply_audio_settings()
