32260d5cfa472f075a1f1250d57dfed95d7a4b77
Liam Kerr <liam.d.kerr@gmail.com>
2020-11-25 01:32:46 +0000

    feat: pickups picking up

---
+++ Pickup.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_Pickup_body_entered(body):
+	if body.is_in_group("player"):
+		if(!body.has_item):
+			body.has_item = true
+			queue_free()
