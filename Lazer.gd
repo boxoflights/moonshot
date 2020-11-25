bf6ff83dfe73a79fcafa607b9f28e39b92bc1519
Liam Kerr <liam.d.kerr@gmail.com>
2020-11-25 23:52:10 +0000

    blaster sound and particles

--- Lazer.gd
+++ Lazer.gd
@@ -3,11 +3,24 @@ extends Area2D
 export (int) var direction = 1
 var speed = 160
 
+var hit = false
+
+var fire_sound = load("res://SFX/lazer-blast.wav")
+
+func _ready():
+	SoundManager.play_2D_sfx(fire_sound,position)
+
 func _physics_process(delta):
-	position.x += direction * speed * delta
+	if(hit):
+		if(!$HitExplosion.emitting):
+			queue_free()
+	else:
+		position.x += direction * speed * delta
 
 func _on_Lazer_body_entered(body):
 	if body.is_in_group("mobs"):
 		body.queue_free()
-	queue_free()
-	
+	hit = true
+	$BulletParticle.emitting = false
+	monitoring = false
+	$HitExplosion.emitting = true
