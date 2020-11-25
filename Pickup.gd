b4b1f437e7c0d33f1c6f8e80aedb2eb26dfe003e
chaegribben <chaegribben@hotmail.com>
2020-11-25 23:36:43 +0000

    Made spawns work using Screen script; Rocket takes off once repaired; Pickups are used on contact with base of Rocket; Lowered jetpack boot time;

--- Pickup.gd
+++ Pickup.gd
@@ -6,5 +6,6 @@ func _ready():
 func _on_Pickup_body_entered(body):
 	if body.is_in_group("player"):
 		if(!body.has_item):
+			print("PICKUP")
 			body.has_item = true
 			queue_free()
--- Rocket.gd
+++ Rocket.gd
@@ -42,3 +42,19 @@ func set_state(new_state):
 			$Smoke/Smoke3.emitting = false
 			$Smoke/Smoke4.emitting = false
 			state = new_state
+
+func launch(delta):
+	position.y -= delta * 128
+	
+func _process(delta):
+	if state == STATES.REPAIRED:
+		launch(delta)
+
+func _on_Rocket_body_entered(body):
+	if body.is_in_group("player"):
+		print("ROCKET")
+		if body.has_item:
+			print("RETURNED")
+			body.has_item = false
+			body.items_returned += 1
+			set_state(state + 1)
